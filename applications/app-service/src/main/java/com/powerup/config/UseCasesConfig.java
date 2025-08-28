f12d36ab69047c810b6a7c5ed7519fd5fd0f3a73
yfernandez <yfernandez@ospinternational.com>
2025-08-28 13:16:59 -0500

    chore: inicializar proyecto solicitudes con Clean Architecture y WebFlux

---
+++ applications/app-service/src/main/java/com/powerup/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.powerup.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.powerup.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
